04d722ca4fc21de4114cc824f17b8ec4f77ce36e
hockey <mr.hockeypack@gmail.com>
2020-05-28 20:17:05 +0800

    提交 146

--- src/main/java/me/hockey/learn/leetcode146/LRUCache.java
+++ src/main/java/me/hockey/learn/leetcode146/LRUCache.java
@@ -14,16 +14,15 @@ import java.util.*;
  *
  */
 class LRUCache {
-
-    //TODO
     private long sort = 0;
-
     private HashMap<Integer,Integer> cacheHash;
     private TreeMap<Long,Integer> sortMap;
+    private HashMap<Integer,Long> keySetMap;
     private int capacity = 0;
 
     public LRUCache(int capacity) {
-        cacheHash = new HashMap<>(capacity);
+        cacheHash = new HashMap<>(capacity+1);
+        keySetMap = new HashMap<>(capacity+1);
         sortMap = new TreeMap();
         this.capacity = capacity;
     }
@@ -32,35 +31,31 @@ class LRUCache {
         int result = cacheHash.getOrDefault(key,-1);
         if(result != -1){
             sort++;
+            sortMap.remove(keySetMap.get(key));
             sortMap.put(sort,key);
+            keySetMap.replace(key,sort);
         }
         return result;
     }
 
     public void put(int key, int value) {
         sort++;
-        int nowHas = cacheHash.size();
-        if(nowHas < this.capacity){
-            if(!cacheHash.containsKey(key)){
-                cacheHash.put(key,value);
-            }else{
-                cacheHash.replace(key,value);
+        int result = cacheHash.getOrDefault(key,-1);
+        //更新
+        if(result != -1){
+            sortMap.remove(keySetMap.get(key));
+            keySetMap.replace(key,sort);
+        } else{
+            int size = cacheHash.size();
+            if (size >= capacity){
+                int removeKey = sortMap.firstEntry().getValue();
+                Long sortValue = keySetMap.remove(removeKey);
+                cacheHash.remove(removeKey);
+                sortMap.remove(sortValue);
             }
-            sortMap.put(sort,key);
-        }else{
-            if(!cacheHash.containsKey(key)){
-                while (sortMap.size() > capacity){
-                    sortMap.remove(sortMap.firstKey());
-                }
-                cacheHash.remove(sortMap.firstEntry().getValue());
-                cacheHash.put(key,value);
-            }else{
-                cacheHash.replace(key,value);
-            }
-            sortMap.put(sort,key);
-            while (sortMap.size() > capacity){
-                sortMap.remove(sortMap.firstKey());
-            }
+            keySetMap.put(key,sort);
         }
+        sortMap.put(sort,key);
+        cacheHash.put(key,value);
     }
 }
