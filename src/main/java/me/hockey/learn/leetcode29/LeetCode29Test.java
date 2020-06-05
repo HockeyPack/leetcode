cb2e0786ad2a57ee2743f91bd65367c332221e78
hockey <mr.hockeypack@gmail.com>
2020-06-05 23:17:49 +0800

    提交 29

---
+++ src/main/java/me/hockey/learn/leetcode29/LeetCode29Test.java
@@ -0,0 +1,54 @@
+/**
+ *
+ */
+package me.hockey.learn.leetcode29;
+
+import java.util.Arrays;
+
+/**
+ *
+ *
+ * 2020/6/5 22:59
+ * @version 1.0.0
+ * @author he.jiepeng
+ *
+ */
+public class LeetCode29Test {
+    public static void main(String[] args) {
+        int[][] source = new int[1][0];
+        System.out.println(Arrays.toString(new Solution().spiralOrder(source)));
+
+        source = new int[1][2];
+        source[0][0] = 1;
+        source[0][1] = 2;
+        System.out.println(Arrays.toString(new Solution().spiralOrder(source)));
+
+
+        source = new int[2][1];
+        source[0][0] = 1;
+        source[1][0] = 2;
+        System.out.println(Arrays.toString(new Solution().spiralOrder(source)));
+
+
+        source = new int[2][2];
+        source[0][0] = 1;
+        source[0][1] = 2;
+        source[1][0] = 4;
+        source[1][1] = 3;
+        System.out.println(Arrays.toString(new Solution().spiralOrder(source)));
+
+
+        source = new int[3][3];
+        source[0][0] = 1;
+        source[0][1] = 2;
+        source[0][2] = 3;
+        source[1][0] = 4;
+        source[1][1] = 5;
+        source[1][2] = 6;
+        source[2][0] = 7;
+        source[2][1] = 8;
+        source[2][2] = 9;
+        System.out.println(Arrays.toString(new Solution().spiralOrder(source)));
+
+    }
+}
---
+++ src/main/java/me/hockey/learn/leetcode29/Solution.java
@@ -0,0 +1,100 @@
+/**
+ *
+ */
+package me.hockey.learn.leetcode29;
+
+/**
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ * 示例 2：
+ *
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 遍历即可
+ *
+ * 2020/6/5 22:24
+ * @version 1.0.0
+ * @author he.jiepeng
+ *
+ */
+class Solution {
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null) {
+            return null;
+        }
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
+        int xLength = matrix[0].length;
+        int yLength = matrix.length;
+        int capacity = xLength * yLength;
+        int[] result = new int[capacity];
+        int maxX = xLength - 1;
+        int maxY = yLength - 1;
+        int minX = 0;
+        int minY = 0;
+        int currentX = 0;
+        int currentY = 0;
+        int currentIndex = 0;
+        int direct = 0;
+
+
+
+        while (currentX >= minX && currentX <= maxX && currentY >= minY && currentY <= maxY) {
+            result[currentIndex] = matrix[currentY][currentX];
+            currentIndex++;
+            if (currentIndex == xLength * yLength) {
+                break;
+            }
+            if (direct == 0) {
+                if (currentX == maxX) {
+                    direct = 1;
+                    currentY++;
+                    minY++;
+                } else {
+                    currentX++;
+                }
+            } else if (direct == 1) {
+                if (currentY == maxY) {
+                    direct = 2;
+                    currentX--;
+                    maxX--;
+                } else {
+                    currentY++;
+                }
+
+            } else if (direct == 2) {
+                if (currentX == minX) {
+                    direct = 3;
+                    currentY--;
+                    maxY--;
+                } else {
+                    currentX--;
+                }
+                continue;
+            } else {
+                if (currentY == minY) {
+                    direct = 0;
+                    currentX++;
+                    minX++;
+                } else {
+                    currentY--;
+                }
+            }
+        }
+
+        return result;
+
+    }
+}
